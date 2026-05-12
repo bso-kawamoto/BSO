cbb7803760efa3c814d21a0f0515f07b385d8827
NaoyaKawamoto <kawamoto@bsobb.net>
2026-05-12 10:05:38 +0900

    Improve project task hierarchy cards

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -456,7 +456,7 @@ function DetailTaskCard({
   const state = getDueState(task.due_date);
 
   return (
-    <article className={`task detailTask ${state ? `due-${state}` : ""}`} id={`task-${task.id}`}>
+    <article className={`task detailTask ${task.task_level === TASK_LEVELS[0] ? "detailTaskMiddle" : "detailTaskChild"} ${state ? `due-${state}` : ""}`} id={`task-${task.id}`}>
       <form action={updateProjectTask} className="detailEditForm">
         <input type="hidden" name="project_id" value={projectId} />
         <input type="hidden" name="id" value={task.id} />
